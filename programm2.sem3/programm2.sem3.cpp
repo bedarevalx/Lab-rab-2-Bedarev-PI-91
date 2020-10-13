4efa0212412362ea9b2a6d97519626a67bc89543
Bedarev <bedarev.axl@mail.ru>
2020-10-13 22:57:36 +0700

    Добавление еще нескольких функций

--- programm2.sem3/programm2.sem3.cpp
+++ programm2.sem3/programm2.sem3.cpp
@@ -1,31 +1,44 @@
 ﻿
 #include <iostream>
 
-typedef struct {
+typedef struct construction {
     float a;
     float b;
     float c;
+    float square;
+    float perimeter;
     void Read();
     void Display();
     void Init(int a, int b, int c);
-}Object;
+    void comaprs(construction1 cons1,construction1 cons2);
+}construction1;
 
-void Object::Read() {
+void construction::Read() {
     printf("Введите стороны объекта:\nДлина = ");
-    scanf("%f", &a);
+    scanf_s("%f", &a);
     printf("Ширина = ");
-    scanf("%f", &b);
+    scanf_s("%f", &b);
     printf("Высота = ");
-    scanf("%f", &c);
+    scanf_s("%f", &c);
 }
-void Object::Display() {
+void construction::Display() {
     printf("\nПараметры объекта\nДлина = %f\nШирина = %f\nВысота = %f", a, b, c);
+}
+void construction::Init(int lenght, int width, int height) {
+    a = lenght;
+    b = width;
+    c = height;
 }
 
 int main()
-
 {
-    Object* a;
     setlocale(LC_ALL, "Rus");
+    construction1 *x1;
+    construction1 *x2;
+    x1->Read();
+    x1->Display();
+    x2->Init(14, 18, 12);
     return 0;
 }
+
+
