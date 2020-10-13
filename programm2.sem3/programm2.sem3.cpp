a6034f05f7125c88cdd9f205b521cf22f21ab6b6
Bedarev <bedarev.axl@mail.ru>
2020-10-13 22:15:25 +0700

    Создание структуры, функции Read и Display

--- programm2.sem3/programm2.sem3.cpp
+++ programm2.sem3/programm2.sem3.cpp
@@ -1,9 +1,31 @@
 ﻿
 #include <iostream>
 
+typedef struct {
+    float a;
+    float b;
+    float c;
+    void Read();
+    void Display();
+    void Init(int a, int b, int c);
+}Object;
+
+void Object::Read() {
+    printf("Введите стороны объекта:\nДлина = ");
+    scanf("%f", &a);
+    printf("Ширина = ");
+    scanf("%f", &b);
+    printf("Высота = ");
+    scanf("%f", &c);
+}
+void Object::Display() {
+    printf("\nПараметры объекта\nДлина = %f\nШирина = %f\nВысота = %f", a, b, c);
+}
+
 int main()
 
 {
+    Object* a;
     setlocale(LC_ALL, "Rus");
     return 0;
 }
